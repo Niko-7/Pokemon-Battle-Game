860ce6aa4cdf1f1f68e19cd4430608a735ce0bb8
Niko-7 <nfloros92@gmail.com>
2020-11-18 14:42:30 +0000

    added trainer method and test

--- tests/trainer.test.js
+++ tests/trainer.test.js
@@ -1,12 +1,33 @@
-
+const { theOriginals } = require("../pokemon");
 const Trainer = require("../trainer");
 
 describe("Trainer", () => {
+  describe("Trainer instance", () => {
     it("Checks that our instance is an object", () => {
       expect(typeof new Trainer()).toBe("object");
     });
     it("Checks that the trainers storage is an empty array", () => {
-        const ash = new Trainer("Ash");
-        expect(ash.storage).toEqual([]);
+      const ash = new Trainer("Ash");
+      expect(ash.storage).toEqual([]);
     });
+  });
+  describe("Trainer Methods", () => {
+    describe("Catch Method", () => {
+      it("checks that the method adds a pokemon to the storage array", () => {
+        const ash = new Trainer("Ash");
+        ash.catch(theOriginals[0]);
+        expect(ash.storage[0]).toEqual({
+          name: "charmander",
+          hp: 100,
+          attack: 20,
+          sound: "char char",
+          move: "flamethrower",
+          type: "fire",
+          weakness: ["water"],
+          strength: ["grass"],
+        });
+      });
+    });
+  });
 });
+
