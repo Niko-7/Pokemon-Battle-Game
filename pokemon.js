7eb58a689e7b36d00a2a181ed0d82bdf14045f5a
Niko-7 <nfloros92@gmail.com>
2020-11-18 12:36:36 +0000

    added talk method and test

--- pokemon.js
+++ pokemon.js
@@ -1,18 +1,22 @@
 class Pokemon {
-    constructor(name, hp, attack, sound, move, type = 'normal') {
-        this.name = name;
-        this.hp = hp;
-        this.attack = attack;
-        this.sound = sound;
-        this.move = move;
-        this.type = type;
-        this.weakness = [];
-        this.strength = [];
-    };
+  constructor(name, hp, attack, sound, move, type = "normal") {
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.sound = sound;
+    this.move = move;
+    this.type = type;
+    this.weakness = [];
+    this.strength = [];
+  }
+
+  talk() {
+    return this.sound;
+  }
 };
 
 
-const charmander = new Pokemon("Charmander", 100, 20 , "char char", "flamethrower", "fire");
+const charmander = new Pokemon("charmander", 100, 20 , "char char", "flamethrower", "fire");
 
 
 module.exports = { Pokemon , charmander}
--- tests/pokemon.test.js
+++ tests/pokemon.test.js
@@ -1,35 +1,42 @@
 const { Pokemon, charmander } = require("../pokemon");
 
 describe("Pokemon", () => {
-  test("Check that our instance is an object", () => {
-    const myPokemon = new Pokemon();
-    expect(typeof myPokemon).toBe("object");
+  describe("Pokemon instances", () => {
+    it("Checks that our instance is an object", () => {
+      const myPokemon = new Pokemon();
+      expect(typeof myPokemon).toBe("object");
+    });
+    it("Checks that our object contains specific keys", () => {
+      const myPokemon = new Pokemon();
+      expect(myPokemon).toHaveProperty(
+        "name",
+        "hp",
+        "attack",
+        "sound",
+        "move",
+        "type"
+      );
+    });
+    it("Checks that type defaults to normal", () => {
+      const myPokemon = new Pokemon();
+      expect(myPokemon.type).toEqual("normal");
+    });
+    it("Checks that our created pokemon has the correct properties", () => {
+      expect(charmander).toEqual({
+        attack: 20,
+        hp: 100,
+        move: "flamethrower",
+        name: "charmander",
+        sound: "char char",
+        strength: [],
+        type: "fire",
+        weakness: [],
+      });
+    });
   });
-  test("Check that our object contains specific keys", () => {
-    const myPokemon = new Pokemon();
-    expect(myPokemon).toHaveProperty(
-      "name",
-      "hp",
-      "attack",
-      "sound",
-      "move",
-      "type"
-    );
-  });
-  test("Check that type defaults to normal", () => {
-    const myPokemon = new Pokemon();
-    expect(myPokemon.type).toEqual("normal");
-  });
-  test("Check that type defaults to normal", () => {
-    expect(charmander).toEqual({
-      attack: 20,
-      hp: 100,
-      move: "flamethrower",
-      name: "Charmander",
-      sound: "char char",
-      strength: [],
-      type: "fire",
-      weakness: [],
+  describe("Pokemon methods", () => {
+      it("Checks that the talk method returns the pokemons sound", () => {
+        expect(charmander.talk()).toEqual("char char")
     });
   });
 });
